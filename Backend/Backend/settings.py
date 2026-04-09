8d66c8d28f47cf89ff710463991697a1ca6c373e
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2026-04-09 10:32:14 +0530

    added deployed frontend url

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -129,12 +129,14 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://banking-app-3ztf.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "https://banking-app-3ztf.vercel.app/",
     "http://127.0.0.1:5173",
 ]
 
